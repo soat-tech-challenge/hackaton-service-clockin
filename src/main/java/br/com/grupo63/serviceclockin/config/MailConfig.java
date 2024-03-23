554dbbe68f4a1c4377d5d863e0f3872bb682cd84
Thiago Bezerra <thiago.bezsilva@gmail.com>
2024-03-23 16:39:23 -0300

    chore: create env variables for mail sender

--- src/main/java/br/com/grupo63/serviceclockin/config/MailConfig.java
+++ src/main/java/br/com/grupo63/serviceclockin/config/MailConfig.java
@@ -1,5 +1,6 @@
 package br.com.grupo63.serviceclockin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,14 +11,26 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${app.mail.host}")
+    private String host;
+
+    @Value("${app.mail.port}")
+    private String port;
+
+    @Value("${app.mail.username}")
+    private String username;
+
+    @Value("${app.mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost("sandbox.smtp.mailtrap.io");
-        mailSender.setPort(2525); // Adjust the port if necessary
-        mailSender.setUsername("4982c5d1dcdb71");
-        mailSender.setPassword("dd3ffab915572c");
+        mailSender.setHost(host);
+        mailSender.setPort(Integer.parseInt(port));
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
